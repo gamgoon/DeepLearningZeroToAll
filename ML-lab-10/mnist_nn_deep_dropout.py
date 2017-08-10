becbc32ba20bf5e569b470f7ec68c117b5bc131e
gamgoon <gamgoon@gmail.com>
2017-08-10 14:14:00 +0900

    lab 11

--- ML-lab-10/mnist_nn_deep_dropout.py
+++ ML-lab-10/mnist_nn_deep_dropout.py
@@ -15,20 +15,38 @@ batch_size = 100
 # input place holders
 X = tf.placeholder(tf.float32, [None, 784])
 Y = tf.placeholder(tf.float32, [None, 10])
+# 학습때는 dropout rate 0.7, 테스트시는 1 이어야한다.
+keep_prob = tf.placeholder(tf.float32)
 
 # weights & bias for nn layers
-W1 = tf.get_variable("W1", shape=[784, 256],
+W1 = tf.get_variable("W1", shape=[784, 512],
     initializer=tf.contrib.layers.xavier_initializer())
-b1 = tf.Variable(tf.random_normal([256]))
+b1 = tf.Variable(tf.random_normal([512]))
 L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
+L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
 
-W2 = tf.get_variable("W2", shape=[256, 256], initializer=tf.contrib.layers.xavier_initializer())
-b2 = tf.Variable(tf.random_normal([256]))
+W2 = tf.get_variable("W2", shape=[512, 512],
+    initializer=tf.contrib.layers.xavier_initializer())
+b2 = tf.Variable(tf.random_normal([512]))
 L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
+L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
 
-W3 = tf.get_variable("W3", shape=[256, 10], initializer=tf.contrib.layers.xavier_initializer())
-b3 = tf.Variable(tf.random_normal([10]))
-hypothesis = tf.matmul(L2, W3) + b3
+W3 = tf.get_variable("W3", shape=[512, 512],
+    initializer=tf.contrib.layers.xavier_initializer())
+b3 = tf.Variable(tf.random_normal([512]))
+L3 = tf.nn.relu(tf.matmul(L2, W3) + b3)
+L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
+
+W4 = tf.get_variable("W4", shape=[512, 512],
+    initializer=tf.contrib.layers.xavier_initializer())
+b4 = tf.Variable(tf.random_normal([512]))
+L4 = tf.nn.relu(tf.matmul(L3, W4) + b4)
+L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
+
+W5 = tf.get_variable("W5", shape=[512, 10],
+    initializer=tf.contrib.layers.xavier_initializer())
+b5 = tf.Variable(tf.random_normal([10]))
+hypothesis = tf.matmul(L4, W5) + b5
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=Y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
@@ -43,7 +61,7 @@ for epoch in range(training_epochs):
 
     for i in range(total_batch):
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-        feed_dict = {X: batch_xs, Y: batch_ys}
+        feed_dict = {X: batch_xs, Y: batch_ys, keep_prob: 0.7}
         c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
         # print(c)
         avg_cost += c / total_batch # ?????
@@ -56,4 +74,5 @@ print('Learning Finished!')
 # Test model and check accuracy
 correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print('Accuracy:', sess.run(accuracy, feed_dict={X: mnist.test.images, Y: mnist.test.labels}))
+print('Accuracy:', sess.run(accuracy, feed_dict={
+    X: mnist.test.images, Y: mnist.test.labels, keep_prob: 1}))
--- ML-lab-10/mnist_nn_xavier_deep.py
+++ ML-lab-10/mnist_nn_xavier_deep.py
@@ -17,18 +17,31 @@ X = tf.placeholder(tf.float32, [None, 784])
 Y = tf.placeholder(tf.float32, [None, 10])
 
 # weights & bias for nn layers
-W1 = tf.get_variable("W1", shape=[784, 256],
+# 넓고 깊게, 하지만 오버피팅.... 그런데 정확도가 더 높게 나와버림.
+W1 = tf.get_variable("W1", shape=[784, 512],
     initializer=tf.contrib.layers.xavier_initializer())
-b1 = tf.Variable(tf.random_normal([256]))
+b1 = tf.Variable(tf.random_normal([512]))
 L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
 
-W2 = tf.get_variable("W2", shape=[256, 256], initializer=tf.contrib.layers.xavier_initializer())
-b2 = tf.Variable(tf.random_normal([256]))
+W2 = tf.get_variable("W2", shape=[512, 512],
+    initializer=tf.contrib.layers.xavier_initializer())
+b2 = tf.Variable(tf.random_normal([512]))
 L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
 
-W3 = tf.get_variable("W3", shape=[256, 10], initializer=tf.contrib.layers.xavier_initializer())
-b3 = tf.Variable(tf.random_normal([10]))
-hypothesis = tf.matmul(L2, W3) + b3
+W3 = tf.get_variable("W3", shape=[512, 512],
+    initializer=tf.contrib.layers.xavier_initializer())
+b3 = tf.Variable(tf.random_normal([512]))
+L3 = tf.nn.relu(tf.matmul(L2, W3) + b3)
+
+W4 = tf.get_variable("W4", shape=[512, 512],
+    initializer=tf.contrib.layers.xavier_initializer())
+b4 = tf.Variable(tf.random_normal([512]))
+L4 = tf.nn.relu(tf.matmul(L3, W4) + b4)
+
+W5 = tf.get_variable("W5", shape=[512, 10],
+    initializer=tf.contrib.layers.xavier_initializer())
+b5 = tf.Variable(tf.random_normal([10]))
+hypothesis = tf.matmul(L4, W5) + b5
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=Y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
